06321caad645b03252534d0bbf2311df99979e8e
Abiodun Oyawale <mayorport09@yahoo.com>
2025-07-27 19:04:12 +0100

    cypress config changes

--- test/ui/cypress.config.js
+++ test/ui/cypress.config.js
@@ -4,7 +4,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: false,
     testIsolation: false
   },
 });
