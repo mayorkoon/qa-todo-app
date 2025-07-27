bb91c835753fad52dd57f8924bcba77f4daabc6c
Abiodun Oyawale <mayorport09@yahoo.com>
2025-07-28 00:31:15 +0100

    test

--- test/UI/cypress/cypress.config.js
+++ test/UI/cypress/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'e2e/**/*.cy.{js,ts}',
+    specPattern: 'e2e/*.cy.{js,ts}',
     supportFile: 'support/e2e.js',
     baseUrl: 'http://localhost:3000', // change if needed
     setupNodeEvents(on, config) {
